05cba03c6307bbe2de473cecadd7002df917254d
Tobias Piechowiak <tpiechowiak@gnresound.com>
2018-11-13 14:58:05 +0100

    Extended regression tree by audiogram classes

--- Regression tree/Tree_analysis_script.R
+++ Regression tree/Tree_analysis_script.R
@@ -6,6 +6,7 @@ library('ggplot2')
 library('partykit')
 library('rpart.plot')
 library('rpart')
+library('dplyr')
 
 path <- '../../Data/data/'
 
@@ -16,7 +17,6 @@ data_SSQ_age_gender <- read.csv(paste0(path,'SSQ_age_gender.csv'))
 data_experience <- read.csv(paste0(path,'HA_experience.csv'))
 
 audiogram <- read.csv2(paste0(path,'audiogramdata.csv'))
-
 
 
 
@@ -77,14 +77,52 @@ ggplot(data_1, aes(x=age.x, y=diff_SSQ)) +
   stat_smooth(method=lm)
 
 
+#audiogram data 
+
+audiogram.left <- read.csv(paste0(path,'Audiogram_left_classified.csv'))
+audiogram.right <- read.csv(paste0(path,'Audiogram_right_classified.csv'))
+
+colnames(audiogram.right)[c(2)] <- c('Age')
+colnames(audiogram.left)[c(2)] <- c('Age')
+
+
+colnames(data_1)[c(1)] = c('ID')
+
+
+#merge audiogram data with the remaining data 
+audiogram.left$ear = c('left')
+audiogram.right$ear = c('right')
+
+audiogram <- rbind(audiogram.left,audiogram.right)
+
+data_2 <- merge(data_1,audiogram,by=c('ID'))
+
+drops <- c('redcap_event_name.y','redcap_event_name.x','age','age.y','sex.y','sex') 
+data_3 <- data_2[,!(names(data_2) %in% drops)]
+
+
+
 #tree model 
-fit_tree <- rpart(diff_SSQ ~ sex + rand_apparat + age + Time_diff_visits + SSQ1 + ha_use_time + ha_usetime_hours_per_day + 
-                    own_ha + ha_dispencer, data = data_1)
+fit_tree <- rpart(diff_SSQ ~ Age + ear + Class + Gender + own_ha + ha_dispencer +
+                    SSQ1 + ha_use_time + ha_usetime_hours_per_day + own_ha + rand_apparat +
+                    Time_diff_visits, data = data_3, maxdepth = 6,
+                    minbucket = 10, 
+                    minsplit = 10)
 rpart.plot(fit_tree, type=2,clip.right.labs = TRUE, branch=1,yesno=2)
 
+#Gender + rand_apparat + Age + Time_diff_visits + SSQ1 + ha_use_time + 
+ # ha_usetime_hours_per_day + own_ha + ha_dispencer + ear + Class
+
+
+
+#classification tree right
+fit_tree <- ctree(diff_SSQ ~ Gender + rand_apparat + Age + Time_diff_visits + SSQ1 + ha_use_time + 
+                    ha_usetime_hours_per_day + own_ha + ha_dispencer + ear + Class,
+                  data = data_3)
+plot(fit_tree,main='Classification based on age and gender right ear')
 
 
 
 #save csv 
-write.csv(data_1,'SSQ_age_gender_TimeVists_manufacturers.csv')
+write.csv(data_3,'SSQDifference_Gender_Age_Time-Diff-Visits_SSQ1_HA-USE-TIME-Hours_OwnHA_HA-dispenser_Ear_AudClass.csv')
 
